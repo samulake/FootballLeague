62da264137c2dc9b974a3dfa94f7809827d9b496
Eryk <Eryk@192.168.0.206>
2017-11-11 16:39:18 +0100

    Set url for leagues page. add HTML view file for leagues

--- Football/src/main/java/pw/dbapp/controller/MainController.java
+++ Football/src/main/java/pw/dbapp/controller/MainController.java
@@ -2,13 +2,20 @@ package pw.dbapp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/home")
 public class MainController {
 	
-	@GetMapping("/home")
+	@GetMapping
 	public String showHomePage() {
 		return "home";
+	}
+	
+	@GetMapping("/leagues")
+	public String showLeagues() {
+		return "leagues";
 	}
 	
 }
