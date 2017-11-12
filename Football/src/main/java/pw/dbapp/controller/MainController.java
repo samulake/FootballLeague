6c23c709a97166d303ce95fe0691d34bca19f8f8
Eryk <Eryk@192.168.0.206>
2017-11-12 18:30:58 +0100

    Implementation of displaying all leagues on simple HTML page

--- Football/src/main/java/pw/dbapp/controller/MainController.java
+++ Football/src/main/java/pw/dbapp/controller/MainController.java
@@ -2,13 +2,11 @@ package pw.dbapp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/home")
 public class MainController {
 	
-	@GetMapping
+	@GetMapping("/home")
 	public String showHomePage() {
 		return "home";
 	}
